26f73b39cd299c9e6490eec17e67c4ec7c8ee06e
Clemens Dehnert <s85492@smb.informatik.htw-dresden.de>
2025-04-25 10:08:47 +0200

    first commit

---
+++ src/main/java/de/pka/flottenmanagement/FlottenmanagementApplication.java
@@ -0,0 +1,13 @@
+package de.pka.flottenmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlottenmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlottenmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/pka/flottenmanagement/FlottenmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package de.pka.flottenmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlottenmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
