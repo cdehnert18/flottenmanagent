ab5807b9b4c40cf86d60b0fb9c02747c9322742f
cdehnert18 <clemensdehnert@gmail.com>
2025-04-25 11:14:56 +0200

    fix entity annotation

--- src/main/java/de/pka/flottenmanagement/model/PositionExec.java
+++ src/main/java/de/pka/flottenmanagement/model/PositionExec.java
@@ -1,27 +1,26 @@
 package de.pka.flottenmanagement.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 
 import java.util.Date;
 
+@Entity
 public class PositionExec {
 
     @Id
-    private Position position;
+    private float latitude;
+    @Id
+    private float longitude;
     @Temporal(TemporalType.TIMESTAMP)
     private Date timeReached;
     private float positionDeviation;
 
-    public PositionExec(Position position, float positionDeviation) {
-        this.position = position;
+    public PositionExec(float positionDeviation) {
         this.timeReached = new Date(System.currentTimeMillis());
         this.positionDeviation = positionDeviation;
-    }
-
-    public Position getPosition() {
-        return position;
     }
 
     public Date getTimeReached() {
