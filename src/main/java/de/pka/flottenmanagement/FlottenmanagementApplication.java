206a528518d4962f003aa4b83c1c6979cc126f61
FranusTheManus <franz.regir@gmx.de>
2025-05-09 10:12:42 +0200

    Typokorrektur SQL Statements Postition -> Position

--- src/main/java/de/pka/flottenmanagement/FlottenmanagementApplication.java
+++ src/main/java/de/pka/flottenmanagement/FlottenmanagementApplication.java
@@ -1,13 +1,28 @@
 package de.pka.flottenmanagement;
 
+import de.pka.flottenmanagement.model.PositionRepository;
+import de.pka.flottenmanagement.model.UGVRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FlottenmanagementApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(FlottenmanagementApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlottenmanagementApplication.class, args);
 	}
 
+}
+
+@Bean
+public CommandLineRunner demo(UGVRepository ugvRepo, PositionRepository posRepo) {
+	return (args) -> {
+		;
+	};
 }
