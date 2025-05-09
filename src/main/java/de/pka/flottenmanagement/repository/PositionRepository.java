01f53380031c0743c005a42d283617849a2ab2f3
s85507 <thebestfighter@gmx.de>
2025-05-09 14:44:35 +0630

    added

--- src/main/java/de/pka/flottenmanagement/repository/PositionRepository.java
+++ src/main/java/de/pka/flottenmanagement/repository/PositionRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.CrudRepository;
 public interface PositionRepository extends CrudRepository<Position, Long>{
     Position findByid(Long id );
 
-    @Query("select pos from Postition Postiion.latitude = ?1 and Posititon.longitude = ?2")
+    @Query("select pos from Position Position.latitude = ?1 and Position.longitude = ?2")
     List<Position> findBylatlong(Integer latitude,Integer longitude);
 }
 
